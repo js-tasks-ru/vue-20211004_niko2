97d15f84ae455f38a2b718344f2be7c8c9993b09
Иван Кубик <ivankubik@Ivans-MacBook-Pro.local>
2022-02-09 20:26:42 +0300

    complete Components

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -3,10 +3,20 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'CounterButton',
 
-  // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
 
+  methods: {
+    incriment() {
+      this.$emit('update:count', this.count + 1);
+    },
+  },
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click="incriment">{{ count }}</button>`,
 });
